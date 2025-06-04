48358deb6a22e7fd279854e9ce3e376579f09746
Your Name <you@example.com>
2025-06-04 09:39:58 +0530

    vite config

--- frontend/chat-app/vite.config.js
+++ frontend/chat-app/vite.config.js
@@ -7,5 +7,9 @@ export default defineConfig({
   plugins: [
     tailwindcss(),
     react(),
-  ]
+  ],
+  server: {
+    host: '0.0.0.0',
+    port: process.env.PORT ? parseInt(process.env.PORT) : 5173
+  }
 })
